581e6e110864a93f8fb6ea7e54ff41f8e5f6e168
Mert Polat <mert.dev@icloud.com>
2023-03-18 12:59:37 +0300

    initial commit

---
+++ RickAndMorty/Scene/Main/View/Cells/HorizontalCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  HorizontalCollectionViewCell.swift
+//  RickAndMorty
+//
+//  Created by mert polat on 10.03.2023.
+//
+
+import UIKit
+
+class HorizontalCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ RickAndMorty/Scene/Main/View/Cells/VerticalCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  VerticalCollectionViewCell.swift
+//  RickAndMorty
+//
+//  Created by mert polat on 10.03.2023.
+//
+
+import UIKit
+
+class VerticalCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
